38a3dce68d2354518d8a696ba51f857e0f1e9e7f
shilad <shilad@gmail.com>
2014-07-25 21:52:43 -0500

    New bash scripts, script to actually run the whois files.

--- src/main/bash/lookup_domains.sh
+++ src/main/bash/lookup_domains.sh
@@ -10,5 +10,5 @@ yum install git python-pip python-psycopg2 -y &&
 rm -rf /root/SourceGeography &&
 git clone https://github.com/shilad/SourceGeography &&
 cd /root/SourceGeography/src/main/python &&
-python ./create_whois_db.py $PSQL_HOST $PSQL_USER $PSQL_PW $PSQL_DB &&
+python ./run_whois_db.py $PSQL_HOST $PSQL_USER $PSQL_PW $PSQL_DB &&
 shutdown -h now
