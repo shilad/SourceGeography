2e2b2722dea9d311ff43e3ffcc11fa51fffd5f1e
shilad <shilad@gmail.com>
2014-07-27 21:57:55 -0500

    Final script in place!

--- src/main/bash/manage_ec2.sh
+++ src/main/bash/manage_ec2.sh
@@ -2,20 +2,24 @@
 
 dir=$(cd $(dirname "$0") && pwd)
 
-echo "dir is $dir"
+NUM_INSTANCES=19
 
-NUM_INSTANCES=2
+while true; do
 
-aws ec2 run-instances \
-    --image-id ami-76817c1e \
-    --count ${NUM_INSTANCES} \
-    --key-name sources-keypair \
-    --user-data file://${dir}/lookup_domains.sh \
-    --instance-type t2.micro \
-    --subnet-id subnet-18171730 \
-    --instance-initiated-shutdown-behavior terminate \
-    --associate-public-ip-address
+    aws ec2 run-instances \
+        --image-id ami-76817c1e \
+        --count ${NUM_INSTANCES} \
+        --key-name sources-keypair \
+        --user-data file://${dir}/lookup_domains.sh \
+        --instance-type t2.micro \
+        --subnet-id subnet-18171730 \
+        --instance-initiated-shutdown-behavior terminate \
+        --associate-public-ip-address
 
-ids=$( aws ec2 describe-instances | grep InstanceId | sed -e 's/.*: "//' | sed -e 's/",//' | tr '\n' ' ')
+    sleep 3000
 
-aws ec2 terminate-instances --instance-ids ${ids}
+    ids=$( aws ec2 describe-instances | grep InstanceId | sed -e 's/.*: "//' | sed -e 's/",//' | tr '\n' ' ')
+    aws ec2 terminate-instances --instance-ids ${ids}
+
+    sleep 300
+done
