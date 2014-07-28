a9e20aff9810043fa3c4c0805403f2a4d4f80ff7
shilad <shilad@gmail.com>
2014-07-27 21:43:18 -0500

    Script to actually run and kill the workers every hour.

---
+++ src/main/bash/manage_ec2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir=$(cd $(dirname "$0") && pwd)
+
+echo "dir is $dir"
+
+NUM_INSTANCES=2
+
+aws ec2 run-instances \
+    --image-id ami-76817c1e \
+    --count ${NUM_INSTANCES} \
+    --key-name sources-keypair \
+    --user-data file://${dir}/lookup_domains.sh \
+    --instance-type t2.micro \
+    --subnet-id subnet-18171730 \
+    --instance-initiated-shutdown-behavior terminate \
+    --associate-public-ip-address
+
+ids=$( aws ec2 describe-instances | grep InstanceId | sed -e 's/.*: "//' | sed -e 's/",//' | tr '\n' ' ')
+
+aws ec2 terminate-instances --instance-ids ${ids}
