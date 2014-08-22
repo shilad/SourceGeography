6e44cfdb9f664b84051b4bd578eaaf36df30023a
shilad <shilad@gmail.com>
2014-08-22 08:34:36 -0500

    Removed urls file (too big!)

---
+++ src/main/java/org/wikibrain/sources/AllUrls.java
@@ -0,0 +1,32 @@
+package org.wikibrain.sources;
+
+import org.wikibrain.utils.WpIOUtils;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Shilad Sen
+ */
+public class AllUrls {
+    public static void main(String args[]) throws IOException {
+        File input = new File("./source_urls.tsv");
+        File output = new File("domains.txt");
+        BufferedWriter writer = WpIOUtils.openWriter(output);
+
+        Set<String> domains = new HashSet<String>();
+        for (Citation cite : new ExtractReader(input)) {
+            String d = cite.getEffectiveDomain2();
+            if (d != null && !domains.contains(d)) {
+                writer.write(d + "\n");
+                domains.add(d);
+            }
+        }
+        writer.close();
+
+        System.out.println("wrote " + domains.size() + " domains");
+    }
+}
