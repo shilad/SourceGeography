6d72e774f0dfc909fe6f43a11e96c4feea895288
shilad <shilad@gmail.com>
2014-08-24 14:27:26 -0500

    Fixed bug.

--- src/main/bash/manage_scraping.sh
+++ src/main/bash/manage_scraping.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 dir=$(cd $(dirname "$0") && pwd)
+REGIONS="us-east-1 us-west-1 us-west-2"
 
 while true; do
 
-    ec2-request-spot-instances ami-76817c1e \
+    ec2-run-instances ami-76817c1e \
         --region us-east-1  \
         --key shilads-aws-keypair \
         --user-data-file ${dir}/scrape_citations.sh \
@@ -12,10 +13,9 @@ while true; do
         --subnet subnet-18171730 \
         --iam-profile myRole \
         --associate-public-ip-address true \
-        --price .02 \
-        --instance-count 100 \
-        --instance-type t1.micro \
-        --valid-until $(date -uv +1H '+%Y-%m-%dT%H:%M:%SZ') ||
+        --instance-initiated-shutdown-behavior terminate \
+        --instance-count 19 \
+        --instance-type t2.micro  ||
             { echo "spot instance request failed!" >&2; exit 1; }
 
     sleep 7050
