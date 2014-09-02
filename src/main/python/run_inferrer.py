43c6ec4167eaa9ae5ae857c03a5ae29d4b98acbe
shilad <shilad@gmail.com>
2014-09-01 23:46:07 -0500

    Handle wikidata urls that are ONLY handled y wikidata.

--- src/main/python/run_inferrer.py
+++ src/main/python/run_inferrer.py
@@ -8,6 +8,7 @@ Infers location of web pages based on four signals:
 import codecs
 import collections
 import sys
+import traceback
 
 import inferrer
 
@@ -18,15 +19,19 @@ inf = inferrer.Inferrer()
 f = codecs.open(PATH_URL_RESULT, 'w', encoding='utf-8')
 counts = collections.defaultdict(int)
 for ui in inf.get_urls():
-    (country, rule) = inf.infer(ui)
-    iso = 'unknown'
-    if country: iso = country.iso
-    tokens = [rule, iso, ui.url, ui.lang, ui.whois, ui.tld, ui.wikidata]
-    for (i, t) in enumerate(tokens):
-        if i > 0: f.write('\t')
-        f.write('null' if t is None else t)
-    f.write('\n')
-    counts[rule] += 1
+    try:
+        (country, rule) = inf.infer(ui)
+        iso = 'unknown'
+        if country: iso = country.iso
+        tokens = [rule, iso, ui.url, ui.lang, ui.whois, ui.tld, ui.wikidata]
+        for (i, t) in enumerate(tokens):
+            if i > 0: f.write('\t')
+            f.write('null' if t is None else t)
+        f.write('\n')
+        counts[rule] += 1
+    except:
+        sys.stderr.write('decoding %s failed: ' % ui.url)
+        traceback.print_exc()
 f.close()
 
 for rule in counts:
