79bb7aa21a5e6278cc82c24355de00a155f992b7
shilad <shilad@gmail.com>
2014-09-03 11:41:37 -0500

    HTML boilerplate

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,11 @@
+$(function(){
+    $('#world-map').vectorMap();
+
+    $("input[name='lang']").autocomplete({ source : ['en', 'fr', 'de', 'uk'], minLength : 0});
+    $("input[name='country']").autocomplete({ source : ['United States', 'France'], minLength : 0});
+
+    $("input[type='text']").bind('focus', function(){ $(this).autocomplete("search"); } );
+
+});
+
 
